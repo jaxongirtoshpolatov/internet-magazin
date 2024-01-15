a1ebd064ff49c8ba5fd19d65c33f47d740946c83
Jaxongir Toshpolatov <jaxongirtoshpolatov76@gmail.com>
2024-01-15 09:18:45 +0500

    finished

--- javaScript/main.js
+++ javaScript/main.js
@@ -23,25 +23,47 @@ console.log(secondHideIcon);
 
 function secondSHowHide() {
   if (secondInput.value == " " || secondInput.value === "") {
-    console.log("Qiymat yo\'q");
-  }else if(secondInput.type === 'password'){
+    console.log("Qiymat yo'q");
+  } else if (secondInput.type === "password") {
     secondInput.setAttribute("type", "text");
-    secondHideIcon.src = './images/icon/view.png';
-  }else {
+    secondHideIcon.src = "./images/icon/view.png";
+  } else {
     secondInput.setAttribute("type", "password");
-    secondHideIcon.src = './images/icon/eyebrow.png';
+    secondHideIcon.src = "./images/icon/eyebrow.png";
   }
 }
 
-let signUpBtn = document.getElementById('signUpBtn');
-let modal = document.getElementById('signUpModal');
-signUpBtn.addEventListener('click', ()=>{
-    signUpBtn.style.display = 'inline-block';
-    modal.style.display = 'none';
-    modal.setAttribute('aria-hidden', 'true');
-    modal.removeAttribute('aria-modal');
-    signUpBtn
-})
+// let signUpBtn = document.getElementById('signUpBtn');
+// let modal = document.getElementById('signUpModal');
+// signUpBtn.addEventListener('click', ()=>{
+//     signUpBtn.style.display = 'inline-block';
+//     modal.style.display = 'none';
+//     modal.setAttribute('aria-hidden', 'true');
+//     modal.removeAttribute('aria-modal');
+//     signUpBtn
+// });
 
+let minus = document.querySelectorAll(".copy-minus");
+let plus = document.querySelectorAll(".copy-plus");
+let progres = document.querySelectorAll(".copy-status");
+let count = 0;
 
-
+// add product
+progres.forEach((status) => {
+  // add product
+  plus.forEach((add) => {
+    add.addEventListener("click", () => {
+      status.innerHTML = count++;
+    });
+  });
+  // remove product
+  minus.forEach((remove) => {
+    remove.addEventListener("click", () => {
+      if (status > 0) {
+        status.innerHTML = count--;
+      } else {
+        status = 0;
+      }
+    });
+  });
+});
